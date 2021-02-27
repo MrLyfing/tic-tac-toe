e283eb837adb395dd478aece54ea00ad91e20104
Eng Philippe <ilyfing@gmail.com>
2021-02-27 17:34:58 -0600

    add constants

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+const ROUTE = {
+  MENU: {
+    PATH: '/',
+    NAME: 'menu'
+  },
+  PICK_TEAM: {
+    PATH: '/pick',
+    NAME: 'pick-team'
+  },
+  GAME: {
+    PATH: '/game',
+    NAME: 'game'
+  }
+}
+
+const TEAM = {
+  X: 'X',
+  Y: 'Y'
+}
+
+export { ROUTE, TEAM }
--- src/main.js
+++ src/main.js
@@ -23,6 +23,7 @@ const store = createStore({
 })
 
 const app = createApp(App)
+
 app.use(router)
 app.use(store)
 app.mount('#app')
